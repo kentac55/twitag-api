1464f35b04e5de4fb7aca79c61d6d08f0aa63656
kentac55 <dev.kentac55@gmail.com>
2019-06-01 14:39:16 +0900

    add twit controller

---
+++ app/com/kc5m/twitag/controllers/TwitController.scala
@@ -0,0 +1,24 @@
+package com.kc5m.twitag.controllers
+
+import com.kc5m.twitag.dto.req.GetTwitsReq
+import com.kc5m.twitag.dto.res._
+import com.kc5m.twitag.usecases.GetTwitsUseCase
+import play.api.libs.json.{Json, Writes}
+import play.api.mvc._
+
+class TwitController(
+    controllerComponents: ControllerComponents
+)(
+    implicit
+    ec: ControllerContext,
+    twitsWrites: Writes[TwitsRes],
+    uc: GetTwitsUseCase
+) extends AbstractController(controllerComponents) {
+  def get(): Action[AnyContent] = Action.async {
+    implicit request: Request[AnyContent] =>
+      uc.exec(GetTwitsReq(Option.empty, Option.empty, Option.empty))
+        .map { twiTags =>
+          Ok(Json.toJson(twiTags))
+        }
+  }
+}
